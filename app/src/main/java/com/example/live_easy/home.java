4566e2a5548498809498ddc4ec2361423dad6e6f
siddhant3646 <siddhant3646@gmail.com>
2021-05-29 19:57:26 +0530

    final commit

--- app/src/main/java/com/example/live_easy/home.java
+++ app/src/main/java/com/example/live_easy/home.java
@@ -1,5 +1,6 @@
 package com.example.live_easy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,7 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
