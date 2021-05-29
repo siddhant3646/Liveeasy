268587194500aed0a35314f7ba204190f1a139cf
siddhant3646 <siddhant3646@gmail.com>
2021-05-29 20:45:32 +0530

    final commit 2

--- app/src/main/java/com/example/live_easy/home.java
+++ app/src/main/java/com/example/live_easy/home.java
@@ -3,15 +3,36 @@ package com.example.live_easy;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home extends AppCompatActivity {
-
+    Button button7,button8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        button7=findViewById(R.id.button7);
+        button8=findViewById(R.id.button8);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(home.this, shipper.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(home.this, transporter.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
